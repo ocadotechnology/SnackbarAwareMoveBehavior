1a7a27834d44e2bd61fd5ea383ee7f1182f19744
Andrew Lord <andrew.lord@ocado.com>
2016-08-04 12:52:51 +0100

    Put the minSdk to 15, which is what is should be for the project. Added showing of Snackbar to the sample to demonstrate the move behaviour.

--- sample/src/main/java/com/ocadotechnology/snackbarawaremovebehavior/SampleActivity.java
+++ sample/src/main/java/com/ocadotechnology/snackbarawaremovebehavior/SampleActivity.java
@@ -17,32 +17,39 @@
 package com.ocadotechnology.snackbarawaremovebehavior;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
+import com.github.andrewlord1990.snackbarbuilder.SnackbarBuilder;
 import com.ocadotechnology.snackbarawaremovebehavior.sample.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class SampleActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_sample);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @OnClick(R.id.fab)
+    void onFabClicked() {
+        new SnackbarBuilder(this)
+                .message(R.string.snackbar_text)
+                .actionText(R.string.snackbar_action_text)
+                .build()
+                .show();
     }
 
 }
